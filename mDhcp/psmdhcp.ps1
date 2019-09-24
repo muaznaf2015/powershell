06413214256010b855cc47cec036498546248736
Ahmad Mu'adz <muaznaf2015@gmail.com>
2019-09-24 17:05:43 +0800

    Update psmdhcp.ps1

--- mDhcp/psmdhcp.ps1
+++ mDhcp/psmdhcp.ps1
@@ -5,3 +5,7 @@ Get-DHCPServer4Scope
 #---First command will run first, second command run need to provide the input, the first run comand will be the input
 Get-DHCPServer4Scope | Get-DHCPServerv4Lease
 
+#---Display all leased IPv4 in scope
+Get-DhcpServerv4Lease -ScopeId <IPv4_address> -AllLeases
+#---Example:
+Get-DhcpServerv4Lease -ScopeId 10.16.40.0 -AllLeases
