39497dd80d0535043852ab26004df31753d940a3
Ahmad Mu'adz <muaznaf2015@gmail.com>
2019-10-04 16:09:09 +0800

    Update psmgmtpc.ps1

--- mMgmt/psmgmtpc.ps1
+++ mMgmt/psmgmtpc.ps1
@@ -2,3 +2,10 @@
 #---Display installed software
 Get-WmiObject win32_product
 Get-CimInstance win32_product
+
+#---sid-500.com recommend to stay with Get-CimInstance **https://sid-500.com/2018/04/02/powershell-how-to-get-a-list-of-all-installed-software-on-remote-computers/
+#---Use Get-Member to explore all properties
+Get-CimInstance win32_product | Get-Member -MemberType Property
+
+#---Display installed software in GridView
+Get-CimInstance win32_product | Select-Object Name, PackageName, InstallDate | Out-GridView
